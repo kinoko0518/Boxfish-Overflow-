7f02cc1855ae46e1a6caa6e11dda469a1f1be43d
Kinokov Shotaskovich <t4keda6osci01a@gmail.com>
2025-10-17 21:46:30 +0900

    I added aquarium outline

--- src/aquarium.rs
+++ src/aquarium.rs
@@ -5,7 +5,9 @@ pub struct AquariumPlugin;
 
 impl Plugin for AquariumPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, highlight_incorrect_bits)
+        app.init_resource::<AquariumResource>()
+            .add_systems(Startup, init_aquarium_resource)
+            .add_systems(Update, highlight_incorrect_bits)
             .add_systems(Update, goal_swaying)
             .add_systems(Update, parse_aquarium);
     }
@@ -13,6 +15,7 @@ impl Plugin for AquariumPlugin {
 
 #[derive(Component)]
 /// アクアリウム上のタイルを識別するコンポーネント
+/// このコンポーネントが付与されていれば新しいシーンが読み込まれたときに消滅する
 pub struct Tiles;
 
 #[derive(Component)]
@@ -44,9 +47,10 @@ pub enum LogiKind {
     Gate,
 }
 
-const WALL_TILESET: &str = "embedded://tile/wall.png";
 const LOGIGATE_TILESET: &str = "embedded://tile/logical_gates.png";
-const GOAL_TILESET: &str = "embedded://tile/goal.png";
+const OUTLINE_TILESET: &str = "embedded://tile/aquarium.png";
+const WALL_SPRITE: &str = "embedded://tile/wall.png";
+const GOAL_SPRITE: &str = "embedded://tile/goal.png";
 
 const TILE_LAYER: f32 = 0.;
 
@@ -66,10 +70,44 @@ pub struct Goal;
 #[derive(Component)]
 pub struct StageCompleted;
 
+#[derive(Resource, Default)]
+pub struct AquariumResource {
+    tile_sprite: Handle<Image>,
+    tile_layout: Handle<TextureAtlasLayout>,
+    outline_sprite: Handle<Image>,
+    outline_layout: Handle<TextureAtlasLayout>,
+    wall_sprite: Handle<Image>,
+    goal_sprite: Handle<Image>,
+}
+
+pub fn init_aquarium_resource(
+    mut resource: ResMut<AquariumResource>,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    resource.tile_sprite = asset_server.load(LOGIGATE_TILESET);
+    resource.tile_layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
+        UVec2::new(16, 16),
+        16,
+        16,
+        None,
+        None,
+    ));
+    resource.outline_sprite = asset_server.load(OUTLINE_TILESET);
+    resource.outline_layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
+        UVec2::new(16, 16),
+        4,
+        4,
+        None,
+        None,
+    ));
+    resource.wall_sprite = asset_server.load(WALL_SPRITE);
+    resource.goal_sprite = asset_server.load(GOAL_SPRITE);
+}
+
 pub fn parse_aquarium(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    tile_resource: Res<AquariumResource>,
     old_tiles: Query<Entity, With<Tiles>>,
     mut on_loaded: EventReader<ConstructAquarium>,
 ) {
@@ -77,20 +115,17 @@ pub fn parse_aquarium(
         for t in old_tiles {
             commands.entity(t).despawn();
         }
-        chars_into_tiles(&aq.content, commands, asset_server, texture_atlas_layouts);
+        chars_into_tiles(&aq.content, commands, tile_resource);
     }
 }
 
-fn chars_into_tiles(
-    aquarium: &str,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let tile_set_image: Handle<Image> = asset_server.load(LOGIGATE_TILESET);
-    let tile_layout = TextureAtlasLayout::from_grid(UVec2::new(16, 16), 16, 16, None, None);
-    let layout_handle = texture_atlas_layouts.add(tile_layout);
+fn chars_into_tiles(aquarium: &str, mut commands: Commands, tile_resource: Res<AquariumResource>) {
+    let aquarium_size = UVec2::new(
+        aquarium.lines().map(|l| l.len()).max().unwrap_or(0) as u32,
+        aquarium.lines().collect::<Vec<&str>>().len() as u32,
+    );
 
+    // 水槽の中身を構築
     for (y, s) in aquarium.lines().rev().enumerate() {
         struct State {
             bitkind: Option<LogiKind>,
@@ -110,9 +145,9 @@ fn chars_into_tiles(
             let bit_common_components = (Tiles, IncorrectBit { remaining: 0 }, coords.clone());
             let from_index = |x: usize, y: usize| -> Sprite {
                 Sprite::from_atlas_image(
-                    tile_set_image.clone(),
+                    tile_resource.tile_sprite.clone(),
                     TextureAtlas {
-                        layout: layout_handle.clone(),
+                        layout: tile_resource.tile_layout.clone(),
                         index: x + y * 16,
                     },
                 )
@@ -160,7 +195,7 @@ fn chars_into_tiles(
                 }
                 'W' => {
                     commands.spawn((
-                        Sprite::from_image(asset_server.load(WALL_TILESET)),
+                        Sprite::from_image(tile_resource.wall_sprite.clone()),
                         Tiles,
                         Collidable,
                         coords,
@@ -168,7 +203,7 @@ fn chars_into_tiles(
                 }
                 'E' => {
                     commands.spawn((
-                        Sprite::from_image(asset_server.load(GOAL_TILESET)),
+                        Sprite::from_image(tile_resource.goal_sprite.clone()),
                         Tiles,
                         Goal,
                         coords,
@@ -198,6 +233,45 @@ fn chars_into_tiles(
             }
         }
     }
+    // 水槽の外側を構築
+    let construct_outline = |index: (usize, usize), pos: IVec2| {
+        (
+            Collidable,
+            TileCoords::from_ivec2(pos),
+            Transform::from_xyz(
+                (pos.x * (TILE_SIZE as i32)) as f32,
+                (pos.y * (TILE_SIZE as i32)) as f32,
+                TILE_LAYER,
+            ),
+            Sprite::from_atlas_image(
+                tile_resource.outline_sprite.clone(),
+                TextureAtlas {
+                    layout: tile_resource.outline_layout.clone(),
+                    index: index.1 * 4 + index.0,
+                },
+            ),
+            Tiles,
+        )
+    };
+    let isize = IVec2::new(aquarium_size.x as i32, aquarium_size.y as i32);
+    // 水槽の左上
+    commands.spawn(construct_outline((0, 0), IVec2::new(-1, isize.y)));
+    // 上 & 下辺
+    for x in 0..isize.x {
+        commands.spawn(construct_outline((1, 0), IVec2::new(x, isize.y)));
+        commands.spawn(construct_outline((1, 2), IVec2::new(x, -1)));
+    }
+    // 右上
+    commands.spawn(construct_outline((2, 0), IVec2::new(isize.x, isize.y)));
+    // 右 & 左辺
+    for y in 0..isize.y {
+        commands.spawn(construct_outline((0, 1), IVec2::new(-1, y)));
+        commands.spawn(construct_outline((2, 1), IVec2::new(isize.x, y)));
+    }
+    // 左下
+    commands.spawn(construct_outline((0, 2), IVec2::new(-1, -1)));
+    // 右下
+    commands.spawn(construct_outline((2, 2), IVec2::new(isize.x, -1)));
 }
 
 pub fn goal_swaying(query: Query<(&mut Transform, &TileCoords), With<Goal>>, time: Res<Time>) {
