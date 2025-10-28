84e92e8cdcb47817caae945cac173646e9dcc419
6osciola <99010199+kinoko0518@users.noreply.github.com>
2025-10-28 15:41:18 +0900

    I refactored stage/construction/each_char.rs

--- src/stage/construction/each_char.rs
+++ src/stage/construction/each_char.rs
@@ -5,17 +5,15 @@ use super::{
 use crate::{prelude::*, stage::SemiCollidable};
 use bevy::prelude::*;
 
+/// This is a support function to generating
+/// logical gate with minimal boiler plate.
 fn generate_logical_gate(
     coords: (TileCoords, Transform),
-    tail_index: (usize, usize),
+    tilemap_index: (usize, usize),
     logikind: LogiKind,
     state: &mut State,
     tile_resource: &Res<AquariumResource>,
-) -> (
-    Sprite,
-    SemiCollidable,
-    ((TileCoords, bevy::prelude::Transform), Tiles),
-) {
+) -> (Sprite, SemiCollidable, TileCoords, Transform, Tiles) {
     let gate_common_components = (coords.clone(), Tiles);
     let sprite: Sprite;
     let do_spawn_head = if let Some(bkind) = state.bitkind {
@@ -24,17 +22,18 @@ fn generate_logical_gate(
         false
     };
     if do_spawn_head {
-        sprite = generate_tile_from_index(tail_index.0 + 1, tail_index.1, tile_resource);
+        sprite = generate_tile_from_index(tilemap_index.0 + 1, tilemap_index.1, tile_resource);
     } else {
-        sprite = generate_tile_from_index(tail_index.0, tail_index.1, tile_resource);
+        sprite = generate_tile_from_index(tilemap_index.0, tilemap_index.1, tile_resource);
         state.bitkind = Some(logikind);
     }
     state.tail_found = !do_spawn_head;
-    (sprite, SemiCollidable, gate_common_components)
+    let ((tile_coords, transform), tile) = gate_common_components;
+    (sprite, SemiCollidable, tile_coords, transform, tile)
 }
 
 fn generate_tile_from_index(x: usize, y: usize, tile_resource: &Res<AquariumResource>) -> Sprite {
-    // 画像のインデックスからSpriteコンポーネントを生成する
+    // Generating a Sprite component from the tilemap index
     Sprite::from_atlas_image(
         tile_resource.tile_sprite.clone(),
         TextureAtlas {
@@ -53,15 +52,14 @@ pub fn interprint_each_char_as_tile(
     tile_resource: &Res<AquariumResource>,
     state: &mut State,
 ) {
-    // 大抵まとめて付与されるTileCoordsと
-    // Transformを1つのタプルにまとめておく
+    // Combining TileCoords and Transform for simplicity
     let coords = (
         TileCoords {
             tile_pos: IVec2::new(x as i32, y as i32),
         },
         Transform::from_xyz((x * TILE_SIZE) as f32, (y * TILE_SIZE) as f32, TILE_LAYER),
     );
-    // Bitに共通のコンポーネント
+    // Common components for a Bit
     let bit_common_components = (
         Tiles,
         IncorrectBit { remaining: 0 },
@@ -69,62 +67,46 @@ pub fn interprint_each_char_as_tile(
         coords.clone(),
     );
 
-    const LOGIKIND_UNDEFINED: &str = "Parse Error: Expected a logigate's tail before any boolean";
+    const LOGIKIND_UNDEFINED_MESSAGE: &str =
+        "Parse Error: Expected a logigate's tail before any boolean";
+    let logigate = match charactor {
+        'A' => Some(((0, 1), LogiKind::And)),
+        'O' => Some(((0, 2), LogiKind::Or)),
+        'N' => Some(((0, 3), LogiKind::Not)),
+        'X' => Some(((0, 4), LogiKind::Xor)),
+        'G' => Some(((2, 0), LogiKind::Equal)),
+        'U' => Some(((0, 5), LogiKind::Undo)),
+        _ => None,
+    };
+    if let Some((index, logikind)) = logigate {
+        commands.spawn(generate_logical_gate(
+            coords,
+            index,
+            logikind,
+            state,
+            tile_resource,
+        ));
+        return;
+    }
+
+    let boolean = match charactor {
+        '0' => Some(((1, 0), false)),
+        '1' => Some(((0, 0), true)),
+        _ => None,
+    };
+    if let Some((index, boolean)) = boolean {
+        commands.spawn((
+            generate_tile_from_index(index.0, index.1, tile_resource),
+            LogiRegister {
+                boolean: boolean,
+                logikind: state.bitkind.expect(LOGIKIND_UNDEFINED_MESSAGE),
+            },
+            bit_common_components,
+        ));
+        return;
+    }
+
     match charactor {
-        'A' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (0, 1),
-                LogiKind::And,
-                state,
-                tile_resource,
-            ));
-        }
-        'O' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (0, 2),
-                LogiKind::Or,
-                state,
-                tile_resource,
-            ));
-        }
-        'N' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (0, 3),
-                LogiKind::Not,
-                state,
-                tile_resource,
-            ));
-        }
-        'X' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (0, 4),
-                LogiKind::Xor,
-                state,
-                tile_resource,
-            ));
-        }
-        'G' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (2, 0),
-                LogiKind::Equal,
-                state,
-                tile_resource,
-            ));
-        }
-        'U' => {
-            commands.spawn(generate_logical_gate(
-                coords,
-                (0, 5),
-                LogiKind::Undo,
-                state,
-                tile_resource,
-            ));
-        }
         'W' => {
             commands.spawn((
                 Sprite::from_image(tile_resource.wall_sprite.clone()),
@@ -139,26 +121,6 @@ pub fn interprint_each_char_as_tile(
                 Tiles,
                 Goal,
                 coords,
-            ));
-        }
-        '0' => {
-            commands.spawn((
-                generate_tile_from_index(1, 0, tile_resource),
-                LogiRegister {
-                    boolean: false,
-                    logikind: state.bitkind.expect(LOGIKIND_UNDEFINED),
-                },
-                bit_common_components,
-            ));
-        }
-        '1' => {
-            commands.spawn((
-                generate_tile_from_index(0, 0, tile_resource),
-                LogiRegister {
-                    boolean: true,
-                    logikind: state.bitkind.expect(LOGIKIND_UNDEFINED),
-                },
-                bit_common_components,
             ));
         }
         _ => (),
